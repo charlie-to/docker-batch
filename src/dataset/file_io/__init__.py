2b81f196d5d5810fa39644201f7bf6e0b2156920
charlie-to <62843186+charlie-to@users.noreply.github.com>
2023-12-07 22:36:43 +0900

    file read module

---
+++ src/dataset/file_io/__init__.py
@@ -0,0 +1,3 @@
+from .read_evt_file import read_evt_file
+
+__all__ = ["read_evt_file"]
---
+++ src/dataset/file_io/read_evt_file.py
@@ -0,0 +1,21 @@
+# Read in the data
+import os
+
+import geopandas as gpd
+import pandas as pd
+from shapely import wkt
+
+DATA_IMPORT_PATH = os.environ.get("DATA_IMPORT_PATH")
+
+
+def read_evt_file(participant_name: str) -> gpd.GeoDataFrame:
+    df = pd.read_csv(DATA_IMPORT_PATH + participant_name + "/processed/evt_all.csv")
+    columns_name = df.columns.values.tolist()
+    columns_name[0] = "time"
+    df.columns = columns_name
+    df["time"] = pd.to_datetime(df.time, format="ISO8601")
+    df["geometry"] = df["geometry"].apply(wkt.loads)
+    df = gpd.GeoDataFrame(df, geometry="geometry")
+    print("**********")
+    df.info()
+    return df
