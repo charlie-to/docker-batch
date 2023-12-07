29fa4601ca0fca7538b40639a536ee054e00e71e
charlie-to <62843186+charlie-to@users.noreply.github.com>
2023-12-07 22:35:59 +0900

    add latex for scienceplots

--- src/dataset/check/check_normalize.py
+++ src/dataset/check/check_normalize.py
@@ -2,56 +2,65 @@ import os
 
 import contextily as cx
 import geopandas as gpd
+import japanize_matplotlib
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import scienceplots  # import: ignore
-from shapely import wkt
 
-plt.style.use(["science", "ieee", "japanese"])
+mpl.use("pgf")
 
-DATA_IMPORT_PATH = os.environ.get("DATA_IMPORT_PATH")
+plt.style.use(["science", "ieee"])
+plt.rcParams["text.usetex"] = True
+plt.rcParams["pgf.texsystem"] = "lualatex"
+plt.rcParams["font.family"] = "Noto Sans CJK JP"
+plt.rcParams["font.serif"] = "Noto Sans CJK JP"
+plt.rcParams["pgf.rcfonts"] = False
+
+DATA_IMPORT_PATH: str = os.environ.get("DATA_IMPORT_PATH")
+PARTICIPANT_NAMES: list[str] = os.environ.get("PARTICIPANT_NAMES").split(" ")
 
 
-def check_normalize() -> None:
+def check_normalize(df: gpd.GeoDataFrame, participant_name: str) -> None:
     """check_normalize
 
     Args:
         df (pd.DataFrame): [description]
         column (str): [description]
     """
-    # Read in the data
-    df = pd.read_csv(DATA_IMPORT_PATH + "raw/processed/evt_all.csv")
-    columns_name = df.columns.values.tolist()
-    columns_name[0] = "time"
-    df.columns = columns_name
-    df["time"] = pd.to_datetime(df.time, format="ISO8601")
-    df["geometry"] = df["geometry"].apply(wkt.loads)
-    df = gpd.GeoDataFrame(df, geometry="geometry")
-    df.info()
-    print("file counts : ", len(df.file_name.unique()))
 
     # Plot the data
     df1 = df.dropna()
     fig, ax = plt.subplots()
     df1.acceleration_x.hist(ax=ax, bins=100)
-    fig.savefig(DATA_IMPORT_PATH + "raw/processed/acceleration_x_hist.png")
+    fig.savefig(
+        DATA_IMPORT_PATH + participant_name + "/processed/acceleration_x_hist.png"
+    )
     plt.close()
     fig, ax = plt.subplots()
     df1.rotated_acceleration_x.hist(ax=ax, bins=100)
-    fig.savefig(DATA_IMPORT_PATH + "raw/processed/rotated_acceleration_x_hist.png")
+    fig.savefig(
+        DATA_IMPORT_PATH
+        + participant_name
+        + "/processed/rotated_acceleration_x_hist.png"
+    )
     plt.close()
     # compare the two columns
     fig, ax = plt.subplots()
     ax.hist(df1.acceleration_x, bins=100, alpha=0.8, label="元データ")
-    ax.hist(df1.rotated_acceleration_x, bins=100, alpha=0.8, label="回転後データ")
+    ax.hist(df1.rotated_acceleration_x, bins=100, alpha=0.8, label="座標回転後")
     ax.legend()
-    fig.savefig(DATA_IMPORT_PATH + "raw/processed/compare_acceleration_x_hist.png")
+    fig.savefig(
+        DATA_IMPORT_PATH
+        + participant_name
+        + "/processed/compare_acceleration_x_hist.png"
+    )
     plt.close()
 
-    fig, ax = plt.subplots(figsize=(30, 30))
-    df1 = df1.set_crs(epsg=4326, allow_override=True)
-    df1 = df1.to_crs(epsg=3857)
-    df1.plot(ax=ax, linewidth=5, color="black", alpha=0.2, markersize=3)
-    cx.add_basemap(ax=ax, source=cx.providers.OpenStreetMap.Mapnik)
-    ax.set_axis_off()
-    fig.savefig(DATA_IMPORT_PATH + "raw/processed/evt_all.png", dpi=300)
+    # fig, ax = plt.subplots(figsize=(20, 20))
+    # df1 = df1.set_crs(epsg=4326, allow_override=True)
+    # df1 = df1.to_crs(epsg=3857)
+    # df1.plot(ax=ax, linewidth=5, color="black", alpha=0.2, markersize=3)
+    # cx.add_basemap(ax=ax, source=cx.providers.OpenStreetMap.Mapnik)
+    # ax.set_axis_off()
+    # fig.savefig(DATA_IMPORT_PATH + participant_name + "/processed/evt_all.png", dpi=300)
--- src/dataset/check_normal.py
+++ src/dataset/check_normal.py
@@ -1,3 +1,11 @@
-from check import check_normalize
+import os
 
-check_normalize()
+from check import check_normalize
+from file_io import read_evt_file
+
+PARTICIPANT_NAMES = os.environ.get("PARTICIPANT_NAMES").split(" ")
+
+if __name__ == "__main__":
+    for participant_name in PARTICIPANT_NAMES:
+        df = read_evt_file(participant_name)
+        check_normalize(df, participant_name)
